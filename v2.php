da7e50984226e01e89bd32695729bd97b566c80c
BLACK-IT <70752866+hackercrazy687@users.noreply.github.com>
2020-09-07 09:21:49 +0000

    Add files via upload

---
+++ v2.php
@@ -0,0 +1,18 @@
+<?php
+
+//LOGIN FACEBOOK
+
+
+echo "<h2>LOGIN FACEBOOK<h2>";
+echo "LOGIN FACEBOOK<br>";
+echo "username:<br>";
+
+$x = "Username:";
+$y = "Password:";
+
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y
+
+ ?>
