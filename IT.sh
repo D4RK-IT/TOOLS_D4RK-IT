60921798317eee00dc15a319b4446fd7eb1d08bf
Mr.J <70752866+D4RK-IT@users.noreply.github.com>
2021-02-10 00:47:49 +0700

    IT.sh

--- IT.sh
+++ IT.sh
@@ -126,7 +126,7 @@ if [ $pil = 6 ]
 then
 curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
 chmod 777 metasploit.sh
-. /metasploit.sh
+./metasploit.sh
 fi
 
 if [ $pil = 7 ]
